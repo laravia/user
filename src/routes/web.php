23550cc9f45851bac1220ddfb783181393bfb71d
manuel <code@leunam.art>
2022-05-04 21:45:44 +0200

    [feat] added redirect

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,13 +1,19 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use Laravia\User\App\Http\Controllers\DashboardController;
 use Laravia\User\App\Http\Controllers\UserController;
 
-$url = "/laravia";
+$route = "/laravia";
+$loginRoute = $route . '/login';
 
-Route::get('/home',[DashboardController::class,'home'])->name('laravia.user::dashboardHome')->middleware(['web','auth']);
+Route::get('/home', [DashboardController::class, 'home'])->name('laravia.user::dashboardHome')->middleware(['web', 'auth']);
 
-Route::get($url.'/dashboard',[DashboardController::class,'index'])->name('laravia.user::dashboard')->middleware(['web','auth']);
-Route::get($url.'/login',[UserController::class,'loginGet'])->name('laravia.user::login')->middleware(['web','guest']);
-Route::post($url.'/login',[UserController::class,'loginPost'])->name('laravia.user::loginPost')->middleware(['web','guest']);
-Route::post($url.'/logout',[UserController::class,'logoutPost'])->name('laravia.user::logout')->middleware(['web','auth']);
+Route::get($route . '/dashboard', [DashboardController::class, 'index'])->name('laravia.user::dashboard')->middleware(['web', 'auth']);
+Route::get($loginRoute, [UserController::class, 'loginGet'])->name('laravia.user::login')->middleware(['web', 'guest']);
+Route::post($loginRoute, [UserController::class, 'loginPost'])->name('laravia.user::loginPost')->middleware(['web', 'guest']);
+Route::post($route . '/logout', [UserController::class, 'logoutPost'])->name('laravia.user::logout')->middleware(['web', 'auth']);
+
+Route::get($route, function () use ($loginRoute) {
+    return redirect($loginRoute);
+});
