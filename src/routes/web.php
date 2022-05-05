076a9592976e4815a34ffbdd2b52a3aa6a4f802c
manuel <code@leunam.art>
2022-05-06 00:48:54 +0200

    [refactor] refactored web for better readability

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,16 +4,13 @@ use Illuminate\Support\Facades\Route;
 use Laravia\User\App\Http\Controllers\DashboardController;
 use Laravia\User\App\Http\Controllers\UserController;
 
-$route = "/laravia";
-$loginRoute = $route . '/login';
-
 Route::get('/home', [DashboardController::class, 'home'])->name('laravia.user::dashboardHome')->middleware(['web', 'auth']);
 
-Route::get($route . '/dashboard', [DashboardController::class, 'index'])->name('laravia.user::dashboard')->middleware(['web', 'auth']);
-Route::get($loginRoute, [UserController::class, 'loginGet'])->name('laravia.user::login')->middleware(['web', 'guest']);
-Route::post($loginRoute, [UserController::class, 'loginPost'])->name('laravia.user::loginPost')->middleware(['web', 'guest']);
-Route::post($route . '/logout', [UserController::class, 'logoutPost'])->name('laravia.user::logout')->middleware(['web', 'auth']);
+Route::get('/laravia/dashboard', [DashboardController::class, 'index'])->name('laravia.user::dashboard')->middleware(['web', 'auth']);
+Route::get('/laravia/login', [UserController::class, 'loginGet'])->name('laravia.user::login')->middleware(['web', 'guest']);
+Route::post('/laravia/login', [UserController::class, 'loginPost'])->name('laravia.user::loginPost')->middleware(['web', 'guest']);
+Route::post('/laravia/logout', [UserController::class, 'logoutPost'])->name('laravia.user::logoutPost')->middleware(['web', 'auth']);
 
-Route::get($route, function () use ($loginRoute) {
-    return redirect($loginRoute);
+Route::get('/laravia', function () {
+    return redirect('/laravia/login');
 });
